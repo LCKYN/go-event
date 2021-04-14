6b7127afdaf24a328598337880233871357a6904
Pawarit Thareechan <root@lckyn.com>
2021-04-14 08:08:57 +0700

    add js file

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('#btnClick').on('click', function () {
+        if ($('#1').css('display') != 'none') {
+            $('#2').html('Here is my dynamic content').show().siblings('div').hide();
+        } else if ($('#2').css('display') != 'none') {
+            $('#1').show().siblings('div').hide();
+        }
+    });
+});
+
+async function paste() {
+    const text = await navigator.clipboard.readText();
+    document.getElementById("testsetst").value = text;
+}
+
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+}
